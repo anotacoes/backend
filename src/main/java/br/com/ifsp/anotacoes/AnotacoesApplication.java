fbc8df45292c7165b4dbcd108e5b20632d8f695e
Mateus da Silva Gasparotto <mateusg@aquarius.cpqd.com.br>
2020-08-24 12:17:07 -0300

    first commit

---
+++ src/main/java/br/com/ifsp/anotacoes/AnotacoesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.anotacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ifsp/anotacoes/AnotacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifsp.anotacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
