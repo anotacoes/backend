9a13c8b95daba189a4e9c5353656c259da899208
Mateus da Silva Gasparotto <mateusg@aquarius.cpqd.com.br>
2020-10-09 13:05:13 -0300

    API do swagger adicionada para documentar a API anotacoes

--- src/main/java/br/com/ifsp/anotacoes/AnotacoesApplication.java
+++ src/main/java/br/com/ifsp/anotacoes/AnotacoesApplication.java
@@ -3,7 +3,10 @@ package br.com.ifsp.anotacoes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class AnotacoesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/com/ifsp/anotacoes/api/config/swagger/SwaggerConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.ifsp.anotacoes.api.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+
+	@Bean
+	public Docket forumApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.ifsp.anotacoes"))
+				.paths(PathSelectors.ant("/**"))
+				.build();
+	}
+}
